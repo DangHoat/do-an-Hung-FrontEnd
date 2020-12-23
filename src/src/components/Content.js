68e9b200e045fa7709cf4c612e4a3876479b7082
DangHoat <danghoat011198@gmail.com>
2020-12-23 18:07:16 +0700

    hii

--- src/src/components/Content.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Content = ({ children }) => <div className="content scrollbar-style-1 scrollbar-width-2x" style={{overflow: "auto", height: window.innerHeight-54-66}}>{children}</div>;
-
-export default Content;
