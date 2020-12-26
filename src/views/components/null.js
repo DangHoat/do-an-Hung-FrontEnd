c829133ed21371381f0863059ae24f6c1afe9ef4
DangHoat <danghoat011198@gmail.com>
2020-12-26 11:54:09 +0700

    haha

---
+++ src/views/components/null.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ const pageNull = () => {
+    return (
+        <div className="text-center">
+          
+        <h1 className="page404-h1">Chọn Tracker để hiển thị!!!!!!!!</h1>
+        <p className="page404-p">
+        Tôi Xin Cảm Ơn!
+        </p>
+      </div>
+    )
+}
+export default pageNull
